d956f86cec474c42a688a1737fc981fd2a6a266f
yanaken <ynsknt0505@gmail.com>
2022-05-21 23:30:52 +0900

    🚚一部ファイルをsrcフォルフダーにまとめる

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
+  srcDir: 'src/',
+
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: "%s - nuxt-amplify-todo",
