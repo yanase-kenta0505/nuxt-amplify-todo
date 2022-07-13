e8c22d01f70cd389039545bea4fb0a77faf487c9
yanasekenta <ynsknt0505@gmail.com>
2022-07-13 23:24:32 +0900

    メールアドレス確認用IDで認証できるように実装

--- src/pages/signup.vue
+++ src/pages/signup.vue
@@ -12,13 +12,17 @@
                 <v-text-field v-model="email" prepend-icon="mdi-account-circle" name="email" label="email" type="text">
                 </v-text-field>
                 <v-text-field v-model="password" id="password" prepend-icon="mdi-lock-outline" name="password"
-                  label="Password" type="password">
+                  label="Password" type="password" v-if="!confirm">
+                </v-text-field>
+                <v-text-field v-model="confirmId" id="confirmId" prepend-icon="mdi-fingerprint" name="confirmId"
+                  label="ID" type="number" v-else>
                 </v-text-field>
               </v-form>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="primary" @click="signup(email, password)">Signup</v-btn>
+              <v-btn color="primary" v-if="!confirm" @click="signup(email, password)">Signup</v-btn>
+              <v-btn color="primary" v-else @click="confirmAuth(email, confirmId)">Confirm</v-btn>
               <v-btn color="primary" to="/login">Login画面へ</v-btn>
             </v-card-actions>
           </v-card>
@@ -29,15 +33,20 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "@nuxtjs/composition-api";
+import { defineComponent, ref, onMounted } from "@nuxtjs/composition-api";
 import { Auth } from "aws-amplify";
 
 export default defineComponent({
   setup(_, context) {
     const email = ref('')
     const password = ref('')
+    const confirmId = ref(null)
+    const confirm = ref(false)
 
 
+    onMounted(() =>{
+      confirm.value = false
+    })
 
     const signup = async (username, password) => {
       try {
@@ -47,12 +56,22 @@ export default defineComponent({
           password,
         })
 
+        confirm.value = !confirm.value
+
         console.log(user)
 
-        context.root.$router.push('/login')
 
       } catch (error) {
         console.log('error sign up', error)
+      }
+    }
+
+    const confirmAuth = async (username, code) => {
+      try {
+        await Auth.confirmSignUp(username, code)
+        context.root.$router.push('/login')
+      } catch (error) {
+        console.log(error)
       }
     }
 
@@ -60,7 +79,10 @@ export default defineComponent({
     return {
       email,
       password,
-      signup
+      signup,
+      confirm,
+      confirmAuth,
+      confirmId
     }
   }
 })
